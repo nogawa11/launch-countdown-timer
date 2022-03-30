068a8fe1b89d6bda972b4d6e5543af98d08a1f95
Nicole Ogawa <nogawa11@gmail.com>
2022-03-30 13:57:46 +0900

    added js functions

---
+++ app.js
@@ -0,0 +1,124 @@
+const days = document.querySelector(".days")
+const hours = document.querySelector(".hours")
+const minutes = document.querySelector(".minutes")
+const seconds = document.querySelector(".seconds")
+const form = document.querySelector(".form")
+const dateInput = document.querySelector("#date")
+const timeInput = document.querySelector("#time")
+const startBtn = document.querySelector(".btn-start")
+const updateBtn = document.querySelector(".btn-update")
+
+const setLaunchInfo = () => {
+  const date = dateInput.value;
+  const time = timeInput.value;
+  const launchInfo = moment(`${date} ${time}}`);
+  return launchInfo;
+}
+
+const setDateTime = () => {
+  const now = moment();
+  dateInput.value = now.add(1, "d").format("YYYY-MM-DD");
+  dateInput.setAttribute("min", now.format("YYYY-MM-DD"));
+  dateInput.setAttribute("max", now.add(3, "months").format("YYYY-MM-DD"));
+  timeInput.value = moment().format("HH:mm")
+}
+
+const formatInfo = (info) => {
+	let formattedInfo = "0";
+	if (info < 10) {
+		formattedInfo += info;
+		return formattedInfo;
+	} else {
+		return info;
+	}
+};
+
+const updateCountdown = (d, h, m, s) => {
+	days.innerText = formatInfo(d);
+	hours.innerText = formatInfo(h);
+	minutes.innerText = formatInfo(m);
+	seconds.innerText = formatInfo(s);
+};
+
+const countdownDisplay = (display, text) => {
+	const main = document.querySelector(".main");
+	main.style.display = display;
+	const header = document.querySelector("h3");
+	header.innerText = text;
+	if (display === "none") {
+		header.classList.add("complete");
+	} else {
+		header.classList.remove("complete");
+	}
+};
+
+const calculate = (launch) => {
+	const now = moment();
+	duration = moment.duration(launch.diff(now));
+	if (duration < 0) {
+		countdownDisplay("none", "Launch is complete");
+	}
+	const daysRemaining = launch.diff(now, "days");
+	const hoursRemaining = duration.hours();
+	const minutesRemaining = duration.minutes();
+	const secondsRemaining = duration.seconds();
+	updateCountdown(daysRemaining, hoursRemaining, minutesRemaining, secondsRemaining);
+};
+
+const findRemainingTime = () => {
+	const localStorageLaunch = localStorage.getItem("launchDate");
+	const launch = setLaunchInfo();
+
+	if (localStorageLaunch) {
+		calculate(moment(localStorageLaunch));
+	} else {
+		calculate(launch);
+	}
+};
+
+const saveLaunch = (launch) => {
+	localStorage.setItem("launchDate", launch);
+};
+
+const countdownInfo = () => {
+	findRemainingTime();
+	const info = setInterval(() => {
+		if (duration <= 0) {
+			clearInterval(info);
+		} else {
+			findRemainingTime();
+		}
+	}, 1000);
+};
+
+const displayForm = (display) => {
+	form.style.display = display;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	setDateTime();
+	if (localStorage.getItem("launchDate") !== null) {
+		displayForm("none");
+		countdownInfo();
+	} else {
+		updateCountdown("0", "0", "0", "0");
+	}
+});
+
+startBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+  launch = setLaunchInfo();
+	saveLaunch(launch);
+	countdownInfo();
+  displayForm('none')
+});
+
+updateBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+  displayForm('block')
+  const storedDate = new Date(localStorage.getItem("launchDate"))
+  const formattedDate = moment(storedDate).format('YYYY-MM-DD')
+  const formattedTime = moment(storedDate).format('HH:mm')
+  dateInput.value = formattedDate;
+  timeInput.value = formattedTime;
+});
